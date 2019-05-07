c68e89d2a0cff28626fd78f8d3ad49a4ff4ce3c0
Anastasia <tvdlover19@gmail.com>
2019-05-07 16:48:38 +0400

    123

---
+++ TankGuiObserver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sharpex2D;
+using Sharpex2D.Rendering;
+using Sharpex2D.Surface;
+
+namespace TankGuiObserver
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MainForm());
+
+            var renderTarget = RenderTarget.Create();     //создаём "цель вывода"
+            renderTarget.Window.Title = $"TankGuiObserver {Application.ProductVersion}";            //название окна
+            renderTarget.Window.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);      //иконка
+            renderTarget.Window.SurfaceLayout = new SurfaceLayout(true, false, true);               //"поверхность" слоёв ~ мб наложение слоёв
+            renderTarget.Window.SurfaceStyle = SurfaceStyle.Fullscreen;                             //стиль наложения
+
+            SGL.Initialize(new Configurator(
+                new BackBuffer(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height),
+                new GuiObserver(), renderTarget));
+
+        }
+    }
+}
